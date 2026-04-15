185e319b00996ec596c1d6ff0c31c098e3d14c38
2025050067 <khk013000@gmail.com>
2026-04-15 18:28:01 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -2,18 +2,17 @@
 
 int main()
 {
-    int answer,guess,cnt=0;
-    scanf("%d",&answer);
+    int answer,guess,cnt=0; //answer,guess는 scanf로 입력받을거라 선언,cnt는 시도횟수 쓸거라서 바로 초기화
+    scanf("%d",&answer); //정답 입력
     do{
-        scanf("%d",&guess);
-        cnt++;
-        if(answer>guess) printf("%d<?\n",guess);
+        scanf("%d",&guess); //추측한 값 입력
+        cnt++; // 추측(시도)했으니 시도 올림
+        if(answer>guess) printf("%d<?\n",guess); 
         else if(answer<guess) printf("%d>?\n",guess);
-        else {
-        printf("%d==?\n",guess);
-        printf("%d",cnt);
-        } 
-    }while(answer!=guess);
+        
+    }while(answer!=guess); //정답 다를땐 돌고 정답일떈 탈출
+    printf("%d==?\n",guess); 
+    printf("%d",cnt);
 
     return 0;
 }
