16b93c96f6773850b701c5dd2a66d092921ecdf4
2025050067 <khk013000@gmail.com>
2026-04-15 17:53:34 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -8,12 +8,11 @@ int main()
         scanf("%d",&guess);
         cnt++;
         if(answer>guess) printf("%d<?",guess);
-        else if(answer==guess) {
+        else if(answer<guess) printf("%d>?",guess);
+        else {
         printf("%d==?\n",guess);
         printf("%d",cnt);
-        }
-        else printf("%d>?",guess);
-            
+        } 
     }while(answer!=guess);
 
     return 0;
